7ce8c6c79dd4bd756fe44b4b7abb0e8eaab26e34
RVodassa <assadov.spb@bk.ru>
2024-06-10 16:29:26 +0300

    0ms

--- leetcodes/0001 Two Sum.go
+++ leetcodes/0001 Two Sum.go
@@ -1,15 +1,15 @@
 package leetcodes
 
 func TwoSum(nums []int, target int) []int {
-	for i, entry := range nums {
-		compliment := target - entry
-		for j, entry2 := range nums {
-			if i != j && entry2 == compliment {
-				return []int{i, j} // Возвращаем индексы, как только нашли пару
-			}
+	seen := map[int]int{}
+
+	for i, num := range nums {
+		complement := target - num
+		if index, ok := seen[complement]; ok {
+			return []int{index, i}
 		}
+		seen[num] = i
 	}
 
-	// Если пара не найдена, возвращаем пустой срез
 	return []int{}
 }
